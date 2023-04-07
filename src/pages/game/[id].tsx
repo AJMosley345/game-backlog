ab02ac52f0189afa7828b8307bbd7025da007456
AJ Mosley <ajmosley345@gmail.com>
2023-04-07 12:34:11 -0400

    Restructure folders

--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -1,9 +1,9 @@
-import prisma from "../../../lib/prisma";
+import prisma from "../../lib/prisma";
 import { GetServerSideProps } from "next";
 import React from "react";
 import Router  from "next/router";
-import { ListProps } from "../../../components/GameList";
-import Layout from "../../../components/Layout";
+import { ListProps } from "../../components/GameList";
+import Layout from "../../components/Layout";
 import { Button } from "@mui/material";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
